67a871fb8b33352d8e865b55ab0288b3b389dc40
Beatriz do Carmo Perez <119524823+Bee-Pirez@users.noreply.github.com>
2023-07-03 18:34:18 -0300

    Delete assets/js directory

--- assets/js/menu.js
+++
@@ -1,8 +0,0 @@
-const navMenu = document.querySelector('.menu_navigation');
-document.addEventListener('click', function (e) {
-    console.log(e.target);
-    if (e.target.classList.contains('menu_button') ||
-        e.target.classList.contains('mavigation_item')) {
-        navMenu.classList.toggle('mobile_menu');
-    }
-})
--- assets/js/scroll.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', () => {
-    const navBar = document.querySelector('navigation');
-    // console.log(window.scrollY);
-    navBar.classList.toggle('active-scroll', window.scrollY >100);
-});
